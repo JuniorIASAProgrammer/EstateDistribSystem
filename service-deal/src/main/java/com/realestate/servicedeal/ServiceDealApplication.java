2495afc9ef50eb2dbddea5581f5293bc9db3ae9a
JuniorIASAProgrammer <vitalikborodavko@gmail.com>
2021-12-27 12:55:13 +0200

    01: structure

---
+++ service-deal/src/main/java/com/realestate/servicedeal/ServiceDealApplication.java
@@ -0,0 +1,13 @@
+package com.realestate.servicedeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDealApplication.class, args);
+    }
+
+}
---
+++ service-deal/src/test/java/com/realestate/servicedeal/ServiceDealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.servicedeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
