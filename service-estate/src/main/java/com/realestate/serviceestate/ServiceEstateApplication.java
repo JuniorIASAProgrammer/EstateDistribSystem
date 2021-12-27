5fc9ddce97cbe00fd4eab8e83fa7eacd95bc3605
JuniorIASAProgrammer <vitalikborodavko@gmail.com>
2021-12-27 13:28:20 +0200

    service-estate-structure

---
+++ service-estate/src/main/java/com/realestate/serviceestate/ServiceEstateApplication.java
@@ -0,0 +1,13 @@
+package com.realestate.serviceestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEstateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEstateApplication.class, args);
+    }
+
+}
---
+++ service-estate/src/test/java/com/realestate/serviceestate/ServiceEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realestate.serviceestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEstateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
